cb16baa54fef598001a6db863e6430a28e256529
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2021-03-28 23:40:16 +0200

    Add initial version of own dwt implementation

--- playground/dwt_example.py
+++ playground/dwt_example.py
@@ -1,10 +1,11 @@
-import os, shutil
-import matplotlib.pyplot as plt
+import os
+import shutil
+from itertools import product, takewhile
 
+import matplotlib.pyplot as plt
+import numpy as np
 from pywt import dwt, dwt2, Wavelet
 from pywt.data import camera
-
-from itertools import product, takewhile
 
 
 def plot_subbands(ll, lh, hl, hh, titles):
@@ -79,6 +80,63 @@ def save_results(results):
         plt.imsave(f"temp/{title}.png", res, cmap=plt.cm.gray)
 
 
+def haar_dwt(image: np.array):
+    # This function assumes that input.length=2^n, n>1
+    output = np.zeros_like(image)
+    length = image.shape[0] // 2
+    while True:
+        length = length // 2
+        for i in range(length):
+            output[i] = image[2 * i] + image[2 * i + 1]
+            output[length + i] = image[2 * i] - image[2 * i + 1]
+
+        if length == 1:
+            return output
+        image = output
+
+
+lut_db1 = [
+    7.071067811865475244008443621048490392848359376884740365883398e-01,
+    7.071067811865475244008443621048490392848359376884740365883398e-01
+]
+
+lut_db2 = [
+    4.829629131445341433748715998644486838169524195042022752011715e-01,
+    8.365163037378079055752937809168732034593703883484392934953414e-01,
+    2.241438680420133810259727622404003554678835181842717613871683e-01,
+    -1.294095225512603811744494188120241641745344506599652569070016e-01
+]
+
+lut_db3 = [
+    3.326705529500826159985115891390056300129233992450683597084705e-01,
+    8.068915093110925764944936040887134905192973949948236181650920e-01,
+    4.598775021184915700951519421476167208081101774314923066433867e-01,
+    -1.350110200102545886963899066993744805622198452237811919756862e-01,
+    -8.544127388202666169281916918177331153619763898808662976351748e-02,
+    3.522629188570953660274066471551002932775838791743161039893406e-02
+]
+
+
+def get_lut(wavelet: str):
+    luts = {
+        "db1": lut_db1,
+        "haar": lut_db1,
+        "db2": lut_db2,
+        "db3": lut_db3
+    }
+    return luts.get(wavelet, 0)
+
+
+def dwt_re(array: np.array, wavelet: str):
+    lut = get_lut(wavelet)
+    out_len = array.shape[0] // 2
+    output = np.zeros(shape=out_len, dtype=np.float64)
+    for i in range(out_len):
+        for j in range(2):
+            output[i] += array[2 * i + j] * lut[(2 - 1) - j]
+    return output
+
+
 def main():
     original = camera()
     results = my_dwt(original, 3)
@@ -86,4 +144,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    dummy = np.array([1, 2, 3, 4])
+    print(dwt_re(dummy, 'haar'))
+    print(dwt(dummy, 'haar')[0])
+    print(dwt(dummy, 'db2')[0])
+    print(np.convolve([2, 1, 1, 2, 3, 4, 4, 3], list(reversed(lut_db2)), mode='valid'))
