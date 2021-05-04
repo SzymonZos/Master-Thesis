bd6c8670e181b5654b6fff5ddaff26d0ac55767b
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2021-05-04 23:59:54 +0200

    Rework running tests to be more conceise and expandable; TODO: investigate pytest_cases

--- ci/activate_venv.sh
+++ ci/activate_venv.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-python3.8 -m venv venv;
-. ./venv/bin/activate;
+python3.8 -m venv venv
+. ./venv/bin/activate
 pip install -r requirements.txt
 
 # https://github.com/travis-ci/travis-ci/issues/5434
--- test/test.py
+++ test/test.py
@@ -1,31 +1,35 @@
-import jpeg2000_test as m
+from typing import List
+from itertools import starmap, product, chain
+
 import numpy as np
 import pytest
+
 from pywt import dwt, dwt2
-from itertools import starmap
+import jpeg2000_test as m
 
 
-ONE_DIMENSIONAL_DATA = [
+WAVELETS = {
+    "bior2.2": m.lut_bior2_2_f
+}
+
+PADDING_MODES = {
+    "symmetric": m.padding_mode.symmetric,
+    "zero": m.padding_mode.zeropad
+}
+
+DWT_FUNCS = {
+    "dwt 1d": (dwt, m.dwt_1d_f),
+    "dwt 2d": (dwt2, m.dwt_2d_f)
+}
+
+DATA_1D = [
     np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
     np.array([1, 2, 3, 4]),
     np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]),
     np.array([1, -2, 3, -4, 5, -6, 7, -8, 9, -10])
 ]
 
-
-@pytest.fixture
-def dwt_rows(request):
-    out = m.dwt_1d_f(request.param, m.lut_bior2_2_f, m.padding_mode.symmetric)
-    ref = dwt(request.param, 'bior2.2')[0]
-    return out, ref
-
-
-@pytest.mark.parametrize("dwt_rows", ONE_DIMENSIONAL_DATA, indirect=True)
-def test_dwt_rows(dwt_rows):
-    assert starmap(np.allclose, dwt_rows)
-
-
-TWO_DIMENSIONAL_DATA = [
+DATA_2D = [
     np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]),
     np.array([[1, 2], [3, 4]]),
     np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]),
@@ -33,13 +37,26 @@ TWO_DIMENSIONAL_DATA = [
 ]
 
 
+def get_test_suit(data: List[np.array], dwt_func: str):
+    return product(data, WAVELETS, PADDING_MODES, [dwt_func])
+
+
+def get_all_test_suits():
+    return chain(
+        get_test_suit(DATA_1D, "dwt 1d"),
+        get_test_suit(DATA_2D, "dwt 2d")
+    )
+
+
 @pytest.fixture
-def dwt_2d(request):
-    out = m.dwt_2d_f(request.param, m.lut_bior2_2_f, m.padding_mode.symmetric)
-    ref = dwt2(request.param, 'bior2.2')[0]
-    return out, ref
+def dwt_fixture(request):
+    data, wavelet, padding_mode, dwt_func = request.param
+    dwt_ref, dwt_impl = DWT_FUNCS[dwt_func]
+    ref = dwt_ref(data, wavelet, padding_mode)[0]
+    out = dwt_impl(data, WAVELETS[wavelet], PADDING_MODES[padding_mode])
+    return ref, out
 
 
-@pytest.mark.parametrize("dwt_2d", TWO_DIMENSIONAL_DATA, indirect=True)
-def test_dwt_cols(dwt_2d):
-    assert starmap(np.allclose, dwt_2d)
+@pytest.mark.parametrize("dwt_fixture", get_all_test_suits(), indirect=True)
+def test_dwt(dwt_fixture):
+    assert starmap(np.allclose, dwt_fixture)
