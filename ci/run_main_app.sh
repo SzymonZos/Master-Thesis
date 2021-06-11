736552fc1385df3bc33d696a61b895ed0f3db03c
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2021-06-11 17:54:16 +0200

    Fix install step of test project

--- ci/run_main_app.sh
+++ ci/run_main_app.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-cmake --build build -- -j "$(nproc)"
+cmake --build build --target jpeg2000 -- -j "$(nproc)"
 ./build/src/jpeg2000 -dq
--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-cmake --build build --target install -- -j "$(nproc)"
+cmake --build build --target jpeg2000_test install -- -j "$(nproc)"
 pytest ./test/test.py
